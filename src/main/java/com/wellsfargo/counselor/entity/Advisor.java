9616e1e30f6eb8d8c33a1793b55946e8de7253e8
NSnusha <18wh1a0496@bvrithyderabad.edu.in>
2025-08-16 21:29:26 -0500

    Have created entities - client, portfolio and security

--- src/main/java/com/wellsfargo/counselor/entity/Advisor.java
+++ src/main/java/com/wellsfargo/counselor/entity/Advisor.java
@@ -1,86 +1,50 @@
 package com.wellsfargo.counselor.entity;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 public class Advisor {
-
     @Id
-    @GeneratedValue()
-    private long advisorId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "advisor_id")
+    private Long advisorId;
 
     @Column(nullable = false)
-    private String firstName;
+    private String name;
 
-    @Column(nullable = false)
-    private String lastName;
-
-    @Column(nullable = false)
-    private String address;
+    @Column(nullable = false, unique = true)
+    private String email;
 
     @Column(nullable = false)
     private String phone;
 
-    @Column(nullable = false)
-    private String email;
+    @OneToMany(mappedBy = "advisor", cascade = CascadeType.ALL)
+    private List<Client> clients;
 
-    protected Advisor() {
+    public Advisor() {}
 
-    }
-
-    public Advisor(String firstName, String lastName, String address, String phone, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.phone = phone;
+    public Advisor(String name, String email, String phone, List<Client> clients) {
+        this.name = name;
         this.email = email;
-    }
-
-    public Long getAdvisorId() {
-        return advisorId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
         this.phone = phone;
+        this.clients = clients;
     }
 
-    public String getEmail() {
-        return email;
-    }
+    public Long getAdvisorId() { return advisorId; }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+
+    public List<Client> getClients() { return clients; }
+    public void setClients(List<Client> clients) { this.clients = clients; }
 }
+
---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,46 @@
+package com.wellsfargo.counselor.entity;
+import jakarta.persistence.*;
+
+import java.security.Security;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "portfolio_id")
+    private Long portfolioId;
+
+    @Column(name = "creation_date", nullable = false)
+    private LocalDate creationDate;
+
+    @OneToOne
+    @JoinColumn(name = "client_id", nullable = false)
+    private Client client;
+
+    @OneToMany(mappedBy = "portfolio", cascade = CascadeType.ALL)
+    private List<Security> securities;
+
+    public Portfolio() {}
+
+    public Portfolio(LocalDate creationDate, Client client, List<Security> securities) {
+        this.creationDate = creationDate;
+        this.client = client;
+        this.securities = securities;
+    }
+
+    public Long getPortfolioId() { return portfolioId; }
+
+    public LocalDate getCreationDate() { return creationDate; }
+    public void setCreationDate(LocalDate creationDate) { this.creationDate = creationDate; }
+
+    public Client getClient() { return client; }
+    public void setClient(Client client) { this.client = client; }
+
+    public List<Security> getSecurities() { return securities; }
+    public void setSecurities(List<Security> securities) { this.securities = securities; }
+
+
+}
